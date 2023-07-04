4c4675ea298a56cd236eb3bb37b773f291568da9
AyushVdoit <ayushjaiswal.vdoit@gmail.com>
2023-07-04 12:59:48 +0530

    "initial commit"

--- practice_2/practice_2_stack.py
+++ practice_2/practice_2_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as lambda_function,
+    aws_apigateway as api
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -9,6 +11,17 @@ class Practice2Stack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        login_lambda = lambda_function.Function(self, id = 'Login_lambda', 
+                                                runtime = lambda_function.Runtime.PYTHON_3_9, 
+                                                handler = "login.handler",code = lambda_function.Code.from_asset("lambdacode\login")
+                                                )
+        login_api = api.LambdaRestApi(self,id='loginapi',rest_api_name='practice_api',handler=login_lambda)
+        
+        logout_lambda = lambda_function.Function(self, id = 'Logout_lambda', 
+                                                runtime = lambda_function.Runtime.PYTHON_3_9, 
+                                                handler = "logout.handler",code = lambda_function.Code.from_asset("lambdacode\logout")
+                                                )
 
         # The code that defines your stack goes here
 
